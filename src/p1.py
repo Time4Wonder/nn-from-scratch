b7a69768c6c56ab52e25dcc27d13c1fad6d452cd
t4w <lorenz.burgard@gmail.com>
2026-09-08 01:28:29 +0200

    simple representation of a 1 Layer model.

--- src/p1.py
+++ src/p1.py
@@ -1,9 +1,20 @@
-from operator import xor
+#example script of one layer with 3 neurons. Without an activations function.
+import numpy as np
 
+# batch size of 3, shape(3,)
+inputs  = [[1,2,3,2.5],
+           [2.0,5.0,-1.0,2.0],
+           [-1.5,2.7, 3.3, -0.8]]
 
-inputs  = [1.2, 5.1,2.1]
-weights =  [3.1, 2.1, 8.7]
-bias = 3
+# weihts of 3 neurons
+weights =  [[0.2,0.8, -0.5, 1.0],
+            [0.5, -0.91,0.26, -0.5],
+            [-0.26,-0.27,0.17,0.87]]
 
-output = inputs[0] * weights[0] + inputs [1] * weights[1] + inputs[2] * weights[2] + bias
+# biases of the 3 neurons
+biases = [2,3, 0.5]
+
+print(weights)
+print(np.array(inputs).T)
+output = np.dot(inputs, np.array(weights).T) + biases
 print(output)
