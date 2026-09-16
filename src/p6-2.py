45e1ecfcf7d323979076360b36ad359026129071
t4w <lorenz.burgard@gmail.com>
2026-09-16 02:00:55 +0200

    activation function Softmax

--- src/p6-2.py
+++ src/p6-2.py
@@ -30,13 +30,22 @@ class Activation_ReLU:
     def forward(self, inputs):
         self.output = np.maximum(0, inputs)
 
-layer1 = Layer_Dense(2,5)
+class Activation_Softmax:
+    def forward(self, inputs):
+        exp_values = np.exp(inputs - np.max(inputs, axis=1, keepdims=True))
+        self.output = exp_values / np.sum(exp_values, axis=1, keepdims=True)
+
+X, y = spiral_data(samples=100, classes=3)
+dense1 = Layer_Dense(2,3)
 activation1 = Activation_ReLU()
+dense2 = Layer_Dense(3,3)
+activation2 = Activation_Softmax()
 
 
-layer1.forward(X)
-activation1.forward(layer1.output)
+dense1.forward(X)
+activation1.forward(dense1.output)
+dense2.forward(activation1.output)
+activation2.forward(dense2.output)
 
-#print(layer1.output)
-print(activation1.output)
+print(activation2.output[:5])
 
